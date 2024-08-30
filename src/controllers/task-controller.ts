6ca545c424e513a51b0f2de6dbf1b8745e60338d
Anushi <jindala982@gmail.com>
2024-08-30 19:55:11 +0530

    common middleware completed and prisma installed

--- src/controllers/task-controller.ts
+++ src/controllers/task-controller.ts
@@ -1,87 +1,109 @@
 import { Request, Response } from "express";
 import { Task } from "../interfaces/task-interface";
+import { BadRequest } from "../errorMessages";
 
 let tasks: Task[] = [];
-
-
 export const getAllTasks = (req: Request, res: Response) => {
-  try{
+  try {
     res.locals.response = {
       data: { result: tasks, totalCount: tasks.length },
-      message: 'message',
-      statusCode: 200
+      message: "success",
+      statusCode: 200,
     };
-  } catch(err: any) {
+  } catch (err: any) {
     res.locals.response = {
       data: {},
-      message: err?.message || err?.toString() || 'Unknown error',
-      statusCode: err?.statusCode || 520
-    };  
-  } 
+      message: err?.message || err?.toString() || "Unknown error",
+      statusCode: err?.statusCode || 520,
+    };
+  }
 };
 
 export const addTask = (req: Request, res: Response) => {
-  const task: Task = {
-    id: tasks.length + 1,
-    title: req.body.title,
-    description: req.body.description,
-    isCompleted: false,
-  };
-  tasks.push(task);
-  res.locals.data = { details: task };
-  res.locals.status = 201;
-  res.locals.message = "success";
+  try {
+    const task: Task = {
+      id: tasks.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      isCompleted: false,
+    };
+    tasks.push(task);
+    res.locals.response = {
+      data: { details: task },
+      message: "success",
+      statusCode: 201,
+    };
+  } catch (err: any) {
+    res.locals.response = {
+      data: {},
+      message: err?.message || err?.toString() || "Unknown error",
+      statusCode: err?.statusCode || 520,
+    };
+  }
 };
 
 export const getTask = (req: Request, res: Response) => {
-  try{
+  try {
     const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
     if (!task?.id) {
-      throw BadRequest('Not found');
+      throw BadRequest("Not found");
     }
     res.locals.response = {
       data: { details: task },
-      message: 'message',
-      statusCode: 200
+      message: "success",
+      statusCode: 200,
     };
-  } catch(err: any) {
+  } catch (err: any) {
     res.locals.response = {
       data: {},
-      message: err?.message || err?.toString() || 'Unknown error',
-      statusCode: err?.statusCode || 520
-    };  
-  } 
-
+      message: err?.message || err?.toString() || "Unknown error",
+      statusCode: err?.statusCode || 520,
+    };
+  }
 };
 
 export const editTask = (req: Request, res: Response) => {
-  const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
-  if (!task) {
-    res.locals.data = { success: false };
-    res.locals.status = 404;
-    res.locals.message = "not found";
-    return;
+  try {
+    const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
+    if (!task?.id) {
+      throw BadRequest("Not found");
+    }
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.isCompleted = req.body.isCompleted || task.isCompleted;
+    res.locals.response = {
+      statusCode: 200,
+      message: "updated successfully",
+      data: { details: task, success: true },
+    };
+  } catch (err: any) {
+    res.locals.response = {
+      data: {},
+      message: err?.message || err?.toString() || "Unknown error",
+      statusCode: err?.statusCode || 520,
+    };
   }
-  task.title = req.body.title || task.title;
-  task.description = req.body.description || task.description;
-  task.isCompleted = req.body.isCompleted || task.isCompleted;
-  res.locals.data = { details: task, success: true };
-  res.locals.status = 200;
-  res.locals.message = "updated successfully";
 };
 
 export const deleteTask = (req: Request, res: Response) => {
-  const index = tasks.findIndex(
-    (reqTask) => reqTask.id === parseInt(req.params.id)
-  );
-  if (index === -1) {
-    res.locals.data = { success: false };
-    res.locals.status = 404;
-    res.locals.message = "not found";
-    return;
+  try {
+    const index = tasks.findIndex(
+      (reqTask) => reqTask.id === parseInt(req.params.id)
+    );
+    if (index == -1) {
+      throw BadRequest("Not found");
+    }
+    tasks.splice(index, 1);
+    res.locals.response = {
+      statusCode: 200,
+      message: "deleted successfully",
+      data: { success: true },
+    };
+  } catch (err: any) {
+    res.locals.response = {
+      data: {},
+      message: err?.message || err?.toString() || "Unknown error",
+      statusCode: err?.statusCode || 520,
+    };
   }
-  tasks.splice(index, 1);
-  res.locals.data = { success: true };
-  res.locals.status = 200;
-  res.locals.message = "deleted successfully";
 };
---
+++ src/errorMessages.ts
@@ -0,0 +1,3 @@
+export const BadRequest = (message: string) => {
+    return { statusCode: 400, message };
+  };
