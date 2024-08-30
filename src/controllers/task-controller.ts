477de5274bcb24c48b88aea11e863729af4a98a9
Anushi <jindala982@gmail.com>
2024-08-30 16:26:42 +0530

    common middleware created

--- src/controllers/task-controller.ts
+++ src/controllers/task-controller.ts
@@ -1,120 +1,83 @@
-import { Response, Request } from "express";
+import { Request, Response } from "express";
 import { Task } from "../interfaces/task-interface";
-import { commonResponse } from "../commonResponse";
+
 let tasks: Task[] = [];
 
+
 export const getAllTasks = (req: Request, res: Response) => {
-  return commonResponse(req, res, {
-    status: 200,
-    message: "success",
-    data: { result: tasks, totalCount: tasks.length },
-  });
-  try {
-    return commonResponse(req, res, {
-      status: 200,
-      message: "success",
-      data: { result: tasks, totalCount: tasks.length },
-    });
-  } catch (error) {
-    return commonResponse(req, res, {
-      status: 500,
-      message: "Internal Server Error",
-      data: { success: false },
-    });
+  console.log(typeof(tasks.length));
+  
+  if (tasks.length > 0) {
+    res.locals.data = { result: tasks, totalCount: tasks.length };
+    res.locals.status = 200;
+    res.locals.message = "success";
+    
+  } else {
+    res.locals.data = { success:true };
+    res.locals.status = 404;
+    res.locals.message = "Nothing to show";
   }
 };
+
 export const addTask = (req: Request, res: Response) => {
-  try {
-    const task: Task = {
-      id: tasks.length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      isCompleted: false,
-    };
-    tasks.push(task);
-    return commonResponse(req, res, {
-      status: 201,
-      message: "success",
-      data: { details: task },
-    });
-  } catch (error) {
-    return commonResponse(req, res, {
-      status: 500,
-      message: "Internal Server Error",
-      data: { success: false },
-    });
-  }
+  const task: Task = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+    isCompleted: false,
+  };
+  tasks.push(task);
+  res.locals.data = { details: task };
+  res.locals.status = 201;
+  res.locals.message = "success";
 };
+
 export const getTask = (req: Request, res: Response) => {
-  try {
-    const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
-    if (!task) {
-      return commonResponse(req, res, {
-        status: 404,
-        message: "Not Found",
-        data: { success: false },
-      });
-    } else {
-      return commonResponse(req, res, {
-        status: 200,
-        message: "success",
-        data: { details: task },
-      });
-    }
-  } catch (error) {
-    return commonResponse(req, res, {
-      status: 500,
-      message: "Internal Server Error",
-      data: { success: false },
-    });
+  const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
+  if (!task) {
+    // res.status(404).json({
+    //   status: 404,
+    //   message: "Not Found",
+    //   data: { success: false },
+    // });
+    res.locals.data = { success: false };
+    res.locals.status = 404;
+    res.locals.message = "Not Found";
+    return;
   }
+  res.locals.data = { details: task };
+  res.locals.status = 200;
+  res.locals.message = "success";
 };
+
 export const editTask = (req: Request, res: Response) => {
-  try {
-    const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
-    if (!task) {
-      return commonResponse(req, res, {
-        status: 404,
-        message: "Not Found",
-        data: { success: false },
-      });
-    } else {
-      (task.title = req.body.title || task.title),
-        (task.description = req.body.description || task.description),
-        (task.isCompleted = req.body.isCompleted || task.isCompleted);
-    }
-  } catch (error) {
-    return commonResponse(req, res, {
-      status: 500,
-      message: "Internal Server Error",
-      data: { success: false },
-    });
+  const task = tasks.find((reqTask) => reqTask.id == parseInt(req.params.id));
+  if (!task) {
+    res.locals.data = { success: false };
+    res.locals.status = 404;
+    res.locals.message = "not found";
+    return;
   }
+  task.title = req.body.title || task.title;
+  task.description = req.body.description || task.description;
+  task.isCompleted = req.body.isCompleted || task.isCompleted;
+  res.locals.data = { details: task, success: true };
+  res.locals.status = 200;
+  res.locals.message = "updated successfully";
 };
+
 export const deleteTask = (req: Request, res: Response) => {
-  try {
-    const index = tasks.findIndex(
-      (reqTask) => reqTask.id === parseInt(req.params.id)
-    );
-    if (index == -1) {
-      return commonResponse(req, res, {
-        status: 404,
-        message: "Not Found",
-        data: { success: false },
-      });
-    } else {
-      tasks.splice(index, 1);
-      return commonResponse(req, res, {
-        status: 200,
-        message: "Deleted Successfully",
-        data: { success: true },
-      });
-    }
-  } catch (error) {
-    return commonResponse(req, res, {
-      status: 500,
-      message: "Internal Server Error",
-      data: { success: false },
-    });
+  const index = tasks.findIndex(
+    (reqTask) => reqTask.id === parseInt(req.params.id)
+  );
+  if (index === -1) {
+    res.locals.data = { success: false };
+    res.locals.status = 404;
+    res.locals.message = "not found";
+    return;
   }
+  tasks.splice(index, 1);
+  res.locals.data = { success: true };
+  res.locals.status = 200;
+  res.locals.message = "deleted successfully";
 };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
 import express, { NextFunction, Request, Response } from "express";
 import routes from "./routes/index";
+
 const app = express();
 const port = 8080;
-app.use(express.json());
-// const middleware = (req: Request, res: Response,next:NextFunction) => {
-//   console.log(res);
 
-//   next()
-//   //  res.send(res);
-// };
+const middleware = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await next();
+    if (!res.headersSent) {
+      const status = res.locals.status || 200;
+      const message = res.locals.message || "success";
+      const data = res.locals.data || null;
+
+      res.status(status).json({
+        status,
+        message,
+        data,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      data: { success: false },
+    });
+  }
+};
+
+app.use(express.json());
+app.use(middleware);
 app.use("/", routes);
+
+// Start the server
 app.listen(port, () => {
-  console.log(`app is running on port:${port}`);
+  console.log(`app is running on port: ${port}`);
 });
